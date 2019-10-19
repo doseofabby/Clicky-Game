949d845e7b2a256d49026c6cc662fcc023890400
Abigail Maldonado <doseofabby@gmail.com>
2019-10-18 22:15:56 -0400

    Updates

---
+++ precache-manifest.e1080a819bbc0bf5284d6899dc170bb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d69c2d119b141291d05cfe2437d1e5d2",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "e8ecd95d9545dbf975bc",
+    "url": "/clicky-game/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "f3a42e8fe38c26d61d91",
+    "url": "/clicky-game/static/css/main.d6cdd0bd.chunk.css"
+  },
+  {
+    "revision": "e8ecd95d9545dbf975bc",
+    "url": "/clicky-game/static/js/2.beee3825.chunk.js"
+  },
+  {
+    "revision": "f3a42e8fe38c26d61d91",
+    "url": "/clicky-game/static/js/main.f2144931.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
